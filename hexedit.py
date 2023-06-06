466d1a3c1677376bd695399a4e15af37835df738
johnnywolinski <johnnywolinski22@gmail.com>
2023-06-05 17:57:24 -0700

    Update hexedit.py
    added a "convert to ascii" button

--- hexedit.py
+++ hexedit.py
@@ -11,9 +11,9 @@ def load_file():
     filepath = filedialog.askopenfilename(initialdir="/", title="Select file")
     if filepath:
         file_path_label.config(text=filepath)
-        threading.Thread(target=process_file, args=(filepath,)).start()
+        threading.Thread(target=process_hex, args=(filepath,)).start()
 
-def process_file(filepath):
+def process_hex(filepath):
     global temp_file_path
     try:
         with open(filepath, 'rb') as f, tempfile.NamedTemporaryFile(delete=False) as temp_file:
@@ -23,9 +23,23 @@ def process_file(filepath):
     except Exception as e:
         messagebox.showerror("Error", str(e))
     else:
-        read_hex_file(temp_file_path)
+        read_temp_file(temp_file_path)
 
-def read_hex_file(filepath):
+def process_ascii(filepath):
+    global temp_file_path
+    try:
+        with open(filepath, 'rb') as f, tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            new_temp_file_path = temp_file.name
+            while chunk := f.read(8192):
+                temp_file.write(chunk.decode(errors='replace').encode())
+            os.remove(temp_file_path)
+            temp_file_path = new_temp_file_path
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+    else:
+        read_temp_file(temp_file_path)
+
+def read_temp_file(filepath):
     hex_dump.delete('1.0', tk.END)
     try:
         with open(filepath, 'r') as f:
@@ -33,7 +47,7 @@ def read_hex_file(filepath):
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
-def save_hex():
+def save_file():
     global temp_file_path
     filepath = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("Text file", "*.txt"), ("All Files", "*.*")))
     if filepath and temp_file_path:
@@ -52,7 +66,10 @@ hex_dump.pack()
 load_button = tk.Button(root, text="Load new file", command=load_file)
 load_button.pack()
 
-save_button = tk.Button(root, text="Dump hex to txt file", command=save_hex)
+convert_button = tk.Button(root, text="Convert to ASCII", command=lambda: threading.Thread(target=process_ascii, args=(file_path_label.cget("text"),)).start())
+convert_button.pack()
+
+save_button = tk.Button(root, text="Save file", command=save_file)
 save_button.pack()
 
 root.mainloop()
